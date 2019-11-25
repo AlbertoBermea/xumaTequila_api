e5a3eb80bd797bd67084782b21e65b63e2e04997
AlbertoBermea <alberto-bermi@hotmail.com>
2019-11-25 16:40:24 -0600

    first commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,12 +1,15 @@
-
 const mongoose = require('mongoose')
 
+
 if ( process.env.NODE_ENV === 'production') {
-  var connectionURL = process.env.DB_CONNECTION_STRING
+  mapbax_token = process.env.connectionURL
 } else {
   const config = require('../config.js')
-  var connectionURL = config.connectionURL
+  connectionURL = config.connectionURL
 }
+
+console.log(connectionURL)
+// revisa tu connectionURL aqui :-)
 
 mongoose.connect( connectionURL, {
   useNewUrlParser: true,
