a7737747ed0fb1718b8dc1a3c172282e71567a94
AlbertoBermea <alberto-bermi@hotmail.com>
2020-01-30 19:37:33 -0600

    Api Calls Cart

---
+++ src/controllers/carts.js
@@ -0,0 +1,77 @@
+const Cart = require('../models/cart')
+
+const createCartItem = function(req, res){
+    const cart = new Cart(req.body)
+    cart.save().then(function() {
+      return res.send(cart)
+    }).catch(function(error) {
+      return res.status(400).send(error)
+    })
+}
+
+const updateCartItem = function(req, res) {
+    const _id = req.params.id
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['numberOfItems']
+    // revisa que los updates enviados sean permitidos, que no envie una key que no permitimos
+    const isValidUpdate = updates.every((update) => allowedUpdates.includes(update))
+  
+    if( !isValidUpdate ) {
+      return res.status(400).send({
+        error: 'Invalid update, only allowed to update: ' + allowedUpdates
+      })
+    }
+    // ya no solo buscamos por id, si no también deberia de ser de el owner
+    // del todo por lo tanto usamos findOneAndUpdate para pasarle mas datos
+    Cart.findByIdAndUpdate(_id, req.body ).then(function(cart) {
+    //Product.findOneAndUpdate({ _id }, req.body ).then(function(product) {
+      if (!cart) {
+        return res.status(404).send({ error: `Cart Item with id ${_id} not found.`})
+      }
+      return res.send(cart)
+    }).catch(function(error) {
+      res.status(500).send({ error: error })
+    })
+  }
+
+  const deleteCartItem = function(req, res) {
+    const _id = req.params.id
+    Cart.findOneAndDelete( _id ).then(function(cart){
+      if(!cart) {
+        return res.status(404).send({ error: `Cart Item with id ${_id} not found.`})
+      }
+      return res.send(cart)
+    }).catch(function(error) {
+      res.status(505).send({ error: error })
+    })
+  }
+
+  const deleteCartsUser = function(req, res) {
+    const user = req.params.user
+    Cart.deleteMany( user ).then(function(cart){
+      if(!cart) {
+        return res.status(404).send({ error: `Cart Item with id ${_id} not found.`})
+      }
+      return res.send(cart)
+    }).catch(function(error) {
+      res.status(505).send({ error: error })
+    })
+  }
+  
+
+  const getCartItemsByCustomer = function(req, res) {
+    // solo podemos hacer GET de los todos del usuario que hizo login
+    const user = req.params.user
+    Cart.find({user}).then(function(cart) {
+      res.send(cart)
+    }).catch(function(error){
+      res.status(500).send(error)
+    })
+  }
+
+  module.exports = {
+      createCartItem: createCartItem,
+      deleteCartItem: deleteCartItem,
+      updateCartItem: updateCartItem,
+      getCartItemsByCustomer: getCartItemsByCustomer
+  }
--- src/routes.js
+++ src/routes.js
@@ -3,23 +3,30 @@ const router = express.Router()
 
 const users = require('./controllers/users.js')
 const products = require('./controllers/products.js')
+const cart = require('./controllers/carts.js')
 const auth = require('./middleware/auth')
 const authAdmin = require('./middleware/authAdmin')
 
-router.get('/users', auth, users.getUser)
+router.get('/users',  users.getUsers)
+router.get('/user',  users.getUser)
 router.post('/login', users.login)
-router.post('/logout', auth, users.logout)
+router.post('/logout',  users.logout)
 router.post('/users', users.createUser)  // signup
-router.patch('/users', auth, users.updateUser)
-router.delete('/users', auth, users.deleteUser)
+router.patch('/users', users.updateUser)
+router.delete('/users', users.deleteUser)
 
 
 router.get('/product/:id',products.getProduct) //buscar producto por id
 router.get('/products',products.getProducts) //buscar todos los productos
 router.get('/productsCat/:category',products.getProductsCat) //buscar los productos de una cateroia especifica
-router.post('/product', authAdmin , products.createProduct) //hacer solo admin // crear un nuevo producto
-router.patch('/product/:id', authAdmin , products.updateProduct) //hacer solo admin // actualizar datos de un producto
-router.delete('/product/:id', authAdmin , products.deleteProduct) //hacer solo admin // eliminar datos de un producto
+router.post('/product',  products.createProduct) //hacer solo admin // crear un nuevo producto
+router.patch('/product/:id',  products.updateProduct) //hacer solo admin // actualizar datos de un producto
+router.delete('/product/:id', products.deleteProduct) //hacer solo admin // eliminar datos de un producto
+
+router.get('/cart/:user', cart.getCartItemsByCustomer)
+router.post('/cart',cart.createCartItem)
+router.patch('/cart/:id',cart.updateCartItem)
+router.delete('/cart/:id',cart.deleteCartItem)
 
 router.get('*', function(req, res) {
   res.send({
