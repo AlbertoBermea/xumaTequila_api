795a46668daed92e39bc0948ba41e56903e88b12
AlbertoBermea <alberto-bermi@hotmail.com>
2020-02-04 01:25:44 -0600

    Add Image Upload

--- src/app.js
+++ src/app.js
@@ -3,8 +3,9 @@ require('./db/mongoose')
 var cors = require('cors');
 
 const router = require('./routes')
+const routerImage = require('./file-upload')
 
-console.log("simon")
+//console.log("simon")
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,7 +13,7 @@ const port = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json()) // parsea a json
 app.use(router)
-
+app.use(routerImage)
 
 app.listen(port, function() {
   console.log('Server up and running on port ' + port)
